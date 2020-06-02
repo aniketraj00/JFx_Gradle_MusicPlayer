7873b9cc1daf91a7f965ff82a568a43c5936eced
Aniket Raj <aniketraj00@gmail.com>
2020-06-02 19:46:08 +0530

    Bug Fixes!

--- src/main/java/com/aniket/musicplayer/Controller.java
+++ src/main/java/com/aniket/musicplayer/Controller.java
@@ -431,7 +431,7 @@ public class Controller {
     @FXML
     public void importTracks() {
         FileChooser chooser = new FileChooser();
-        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP3 Files", "*.mp3","*.wav"));
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Music Files", "*.mp3","*.wav"));
         List<File> files = chooser.showOpenMultipleDialog(this.mainWindow.getScene().getWindow());
         if (files != null) {
             this.importService.setFiles(files);
